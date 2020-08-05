e7f4a29623cbd172a9370940d7883f80c93e42cb
Armin Zundel <armin.zundel@gmail.com>
2020-08-05 15:31:45 +0200

    Feedback funktioniert endlich

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -15,6 +15,20 @@ module.exports = {
 
       'desktop': '1900px',
       // => @media (min-width: 1900px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ...}
+
+	'xl': '1280px',
+	// => @media (min-width: 1280px) { ... }
+
+
     },
   },
 
--- lib/kaytest_web/live/memory_live.ex
+++ lib/kaytest_web/live/memory_live.ex
@@ -4,90 +4,79 @@ defmodule KaytestWeb.MemoryLive do
   
   @impl true
   def mount(_params, _session, socket) do
+
     if connected?(socket) do
       :timer.send_interval(1000, self(), :update)
     end
-
-    if connected?(socket) do
-      :timer.send_interval(1000, self(), :tick)
-    end
-
-    :timer.send_interval(1000, self(), :update)
-    
     socket = assign_feedback(socket)
     {:ok, socket}
   end
 
   @impl true
   def render(assigns) do
-    ~L"""
+   ~L"""
+      <h1 class="text-yellow-500 text-5xl font-bold text-center">Memory Challenge</h1>
 
+      <div class="w-full space-x-4 bg-grey rounded overflow-hidden  border-gray-400 p-10 flex justify-center" >
+        <div class=" rounded   w-3/4">
+       <div class="w-full grid grid-cols-3  desktop:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6"> 
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 <div class="column">    <img src="/images/back.png"></div>
+	 </div>
+        </div> 
 
-     <h1 class="text-yellow-500 text-5xl font-bold text-center">Memory Challenge</h1>
+       <div class="space-y-2 w-1/4">
+        <button class="bg-gray-200 hover:bg-gray-200  p-3 py-2 w-full rounded-lg text-gray-700 gap-4 xl:text-3xl desktop:text-yellow-500">
+          <div class="grid grid-cols-2 gap-4">
+            <div>Difficulty:</div><div><%= @difficulty %></div>
+	  </div>
+        </button>
+        <button class="bg-gray-200 hover:bg-gray-200  p-3 py-2 w-full rounded-lg text-gray-700 xl:text-3xl">
+         <div class="grid grid-cols-2 gap-4">
+            <div>Points:</div><div><%= @points %> </div>
+         </div>
+        </button>
+        <button class="bg-gray-200 hover:bg-gray-200  p-3 py-2 w-full rounded-lg text-gray-700 xl:text-3xl">
+         <div class="grid grid-cols-2 gap-4">
+           <div>Progress:</div><div><%= @progress %></div>
+         </div>
+        </button>
+        <button class="bg-gray-200 hover:bg-gray-200  p-3 py-2 w-full rounded-lg text-gray-700 xl:text-3xl">
+         <div class="grid grid-cols-2 gap-4">
+           <div>Remaining:</div><div><%= @progress %></div>
+         </div>
+        </button>
+        <button class="bg-gray-200 hover:bg-gray-200  p-3 py-15 w-full rounded-lg text-gray-700">
+           Here is some additional text that will allow us to inform 
+	   the user about progress or others. Whatever you do, you will always be my number 1 
+        </button>
 
-     <div class="space-x-10 bg-grey rounded overflow-hidden  border-l  border-gray-400 p-4 p-10 flex justify-center" style="">
-       <div class=" rounded overflow-hidden shadow-lg w-2/3">
+      </div>
+      </div>
 
-         <div class="grid grid-cols-4 sm:grid-cols-5 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-6">
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 <div class="column">    <img src="/images/back.png"></div>
-	 
-    </div>
-    </div> 
-
-
-    <div class="space-y-2 w-1/3">
-
-    <button class="bg-gray-200 hover:bg-gray-200  p-3 py-2 w-full rounded-lg text-gray-700 gap-4 xl:text-3xl desktop:text-yellow-500">
-    <div class="grid grid-cols-2 gap-4">
-    <div>Difficulty:</div><div><%= @difficulty %></div>
-    </button>
-    <button class="bg-gray-200 hover:bg-gray-200  p-3 py-2 w-full rounded-lg text-gray-700 xl:text-3xl">
-    <div class="grid grid-cols-2 gap-4">
-    <div>Points:</div><div><%= @points %> </div>
-    </div>
-    </button>
-    <button class="bg-gray-200 hover:bg-gray-200  p-3 py-2 w-full rounded-lg text-gray-700 xl:text-3xl">
-    <div class="grid grid-cols-2 gap-4">
-    <div>Progress:</div><div><%= @progress %></div>
-    </div>
-    </button>
-
-    <button class="bg-gray-200 hover:bg-gray-200  p-3 py-15 w-full rounded-lg text-gray-700">
-    Here is some additional text that will allow us to inform the user about progress or others. Whatever you do, you will always be my number 1 
-    </div>
-    </button>
-
-    </div>
-
-
-    </div></div></div>
-
-      <button phx-click="refresh">
-        <img src="images/back.png">
-        Refresh
-      </button>
-
-    """
-    
+     <button phx-click="refresh">
+      <img src="images/back.png">
+      Refresh
+     </button>
+     """
   end
 
   
@@ -110,13 +99,9 @@ defmodule KaytestWeb.MemoryLive do
     end
   end
 
+  @impl true
+  
   def handle_info(:update, socket) do
-    socket = assign_feedback(socket)
-    IO.puts("this function was called")
-    {:noreply, socket}
-  end
-
-  def handle_info(:tick, socket) do
     socket = assign_feedback(socket)
     {:noreply, socket}
   end
